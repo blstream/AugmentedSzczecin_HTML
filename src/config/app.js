f51de3200788c0c8ed0a3b2487442fc565ec4cac
dawidglosny <dawidglosny@gmail.com>
2015-04-01 14:15:29 +0200

    komentarze

--- src/config/app.js
+++ src/config/app.js
@@ -1,5 +1,10 @@
 var AugmentedSzczecin = angular.module('AugmentedSzczecin',['ui.router']);
-
+/**
+ * [description]
+ * @param  {[type]} $stateProvider        [description]
+ * @param  {[type]} $urlRouterProvider){                 $urlRouterProvider.otherwise("/map");    $stateProvider        .state('map',{        url: "/map",        templateUrl: "src/templates/map.html",        controller: 'MapController'    });} [description]
+ * @return {[type]}                       [description]
+ */
 AugmentedSzczecin.config(function ($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise("/map");
     $stateProvider
