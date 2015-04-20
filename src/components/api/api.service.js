a19425d59858c82505297b39b4a325b1363c0e98
Wiktor Rutka <wrutek@wrutkowecudo.local>
2015-04-20 21:01:24 +0200

    [api] assing 'this' to 'that'
     it is used to access higher context 'this'
     from lower context

--- src/components/api/api.service.js
+++ src/components/api/api.service.js
@@ -1,8 +1,12 @@
 AugmentedSzczecin.service('apiService', function($http){
-    var apiLocation= 'http://private-anon-1813a5f7c-patronage2015.apiary-mock.com/pois';
+    var that = this,
+        apiLocation= 'http://private-anon-1813a5f7c-patronage2015.apiary-mock.com/pois';
+
+    that.getPoi = [];
+
     $http.get(apiLocation).
         success(function(data, status, headers, config){
-           this.getPoi= data;
+           that.getPoi = data;
         }).
         error(function(data, status, headers, config){
             //here will appear Error Handling someday...
