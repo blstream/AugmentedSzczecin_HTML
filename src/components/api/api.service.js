f65dc649ec3a509cab1987d912f702f8def94753
spieszku <spiechowicz.a@gmail.com>
2015-04-27 22:24:04 +0200

    added uris for events

--- src/components/api/api.service.js
+++ src/components/api/api.service.js
@@ -19,19 +19,42 @@ AugmentedSzczecin.service('apiService', function($http){
         return $http.get(poisUri)
     };
     /** create new POI */
-    that.createPoi = function () {
-        return $http.post(poisUri)
+    that.createPoi = function (data) {
+        return $http.post(poisUri, data)
     };
     /** retrieve single POI */
     that.getPoiById = function (id) {
         return $http.get(poisUri + '/' + id)
     };
     /**update single POI */
-    that.updatePoi = function (id) {
-        return $http.put(poisUri + '/' + id)
+    that.updatePoi = function (id, data) {
+        return $http.put(poisUri + '/' + id, data)
     };
     /**delete single POI */
     that.deletePoi = function (id) {
         return $http.delete(poisUri + '/' + id)
+    };
+    //uri for all events
+    var eventsUri = apiLocation + 'events';
+
+    /** get all events */
+    that.getEvents = function () {
+        return $http.get(eventsUri)
+    };
+    /** add new event */
+    that.addEvent = function (data) {
+        return $http.post(eventsUri, data)
+    };
+    /**retrieve single event */
+    that.retrieveEvent = function (id) {
+        return $http.get(eventsUri + '/' + id)
+    };
+    /** update single event */
+    that.updateEvent = function (id, data) {
+        return $http.put(eventsUri + '/' + id, data)
+    };
+    /**delete single event */
+    that.deleteEvent = function (id) {
+        return $http.delete(eventsUri + '/' + id)
     };
 });
