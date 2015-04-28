a848ead1df42ebc188c74d54d35f124b8ce7707c
Wiktor Rutka <wiktor.rutka@blstream.com>
2015-04-28 17:17:29 +0200

    [#AUGMENTEDSZN-240] login form added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 var AugmentedSzczecin = angular.module('AugmentedSzczecin',['ui.router']);
 
 AugmentedSzczecin.config(function ($stateProvider, $urlRouterProvider){
-    $urlRouterProvider.otherwise("/map");
-    $stateProvider
-        .state('map',{
-        url: "/map",
-        templateUrl: "src/modules/maps/maps.tmpl.html",
-        controller: 'MapController'
+  $urlRouterProvider.otherwise("/map");
+  $stateProvider
+    .state('map',{
+      url: "/map",
+      templateUrl: "src/modules/maps/maps.tmpl.html",
+      controller: 'MapController'
+    })
+    .state('login', {
+      url: "/login",
+      templateUrl: "src/modules/auth/login.tmpl.html",
+      controller: "LoginController"
     });
 });
 
