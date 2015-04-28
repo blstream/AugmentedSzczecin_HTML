eed9a83ddd6fce11638313f47e20f077de443a6a
Wiktor Rutka <wiktor.rutka@blstream.com>
2015-04-28 17:34:12 +0200

    [#AUGMENTEDSZN-240] save user into cookie

--- src/modules/auth/login.controller.js
+++ src/modules/auth/login.controller.js
@@ -2,10 +2,14 @@
  * @author Wiktor Rutka <wiktor.rutka@blstream.com>
  */
 
-AugmentedSzczecin.controller('LoginController', ['$scope', function($scope) {
+AugmentedSzczecin.controller('LoginController', ['$scope', 'ipCookie', function($scope, ipCookie) {
   $scope.user = {};
 
+  /**
+   * Save username and password into cookie
+   * @param user {Object} - user.email, user.password
+   */
   $scope.loginAction = function (user) {
-    console.log(user.email, user.password);
+    ipCookie('user', user);
   }
 }]);
