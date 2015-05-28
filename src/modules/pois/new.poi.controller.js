8050344ca641182bc8d47bd07952686ef1325564
spieszku <spiechowicz.a@gmail.com>
2015-05-28 22:31:24 +0200

    [AUGMENTEDSZN-320] parsing opening days

--- src/modules/pois/new.poi.controller.js
+++ src/modules/pois/new.poi.controller.js
@@ -1,9 +1,21 @@
 AugmentedSzczecin.controller('AddPoiController', ['$scope', 'apiService', function($scope, apiService) {
     $scope.addPoi = function(poi) {
+        $scope.newPoi = poi;
+        /**
+         * Saves checked days of the week as array with their names
+         */
+        daysMappings = { "monday": "MONDAY", "tuesday":"TUESDAY", "wednesday":"WEDNESDAY", "thursday": "THURSDAY", "friday":"FRIDAY", "saturday":"SATURDAY", "sunday":"SUNDAY"};
+        daysList = [];
+        for(day in $scope.newPoi['opening']['day']) {
+            if ($scope.newPoi['opening']['day'][day]) {
+                daysList.push(daysMappings[day]);
+            }
+        }
+        $scope.newPoi.opening.day = daysList;
         $scope.newPoi = angular.toJson(poi);
+        /**
+         * post new poi to server
+         */
         apiService.addPlace($scope.newPoi);
     };
-    $scope.days = [
-        'poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota', 'niedziela'
-    ]
 }]);
