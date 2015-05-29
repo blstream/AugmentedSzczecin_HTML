f45f1e7dce3b91807b6532fb0f7b1007ab69db68
spieszku <spiechowicz.a@gmail.com>
2015-05-29 16:18:37 +0200

    [AUGMENTEDSZN-320] saving time as string

--- src/modules/pois/new.poi.controller.js
+++ src/modules/pois/new.poi.controller.js
@@ -1,4 +1,4 @@
-AugmentedSzczecin.controller('AddPoiController', ['$scope', 'apiService', function($scope, apiService) {
+AugmentedSzczecin.controller('AddPoiController', ['$scope', '$filter', 'apiService', function($scope, $filter, apiService) {
     $scope.addPoi = function(poi) {
         $scope.newPoi = poi;
         /**
@@ -12,6 +12,22 @@ AugmentedSzczecin.controller('AddPoiController', ['$scope', 'apiService', functi
             }
         }
         $scope.newPoi.opening.day = daysList;
+
+        /**
+         * get time from input and save it as string
+         */
+        var openTime = Date.parse($scope.newPoi.opening.open);
+        openTime = new Date(openTime);
+        var openHours = openTime.getHours();
+        var openMinutes = openTime.getMinutes();
+        $scope.newPoi.opening.open = openHours +':' + openMinutes;
+
+        var closeTime = Date.parse($scope.newPoi.opening.close);
+        closeTime = new Date(closeTime);
+        var closeHours = closeTime.getHours();
+        var closeMinutes = closeTime.getMinutes();
+        $scope.newPoi.opening.close = closeHours + ":" + closeMinutes;
+
         $scope.newPoi = angular.toJson(poi);
         /**
          * post new poi to server
