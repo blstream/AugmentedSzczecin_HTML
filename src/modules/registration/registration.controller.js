6d95459cad5b67f0da2d5496bb76c8af35c66dd6
spieszku <spiechowicz.a@gmail.com>
2015-06-10 22:47:39 +0200

    [AugmentedSzn-239] comparing passwords

--- src/modules/registration/registration.controller.js
+++ src/modules/registration/registration.controller.js
@@ -1,7 +1,10 @@
 AugmentedSzczecin.controller('RegistrationController', ['$scope', 'apiService', '$state', function($scope, apiService, $state) {
     $scope.signUp = function(user) {
         $scope.newUser = user;
-        console.log($scope.newUser);
+            if (angular.equals(user.password, user.pswrd)) {
+            } else {
+                alert("Hasła nie są jednakowe");
+            };
         apiService.addUser($scope.newUser)
             .success(function(data, status, headers, config){
                 $scope.newUser = data;
